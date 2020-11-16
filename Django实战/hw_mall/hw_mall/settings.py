063914c4b6d3738b43ad33f7e2570e579b56dc39
Ivan-will <yuweiwei_dev@gmail.com>
2020-11-16 15:40:33 +0800

    增加简单的前台模块

--- Django实战/hw_mall/hw_mall/settings.py
+++ Django实战/hw_mall/hw_mall/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myadmin.adminmiddleware.AdminMiddleware'  # 注册中间件
 ]
 
 ROOT_URLCONF = 'hw_mall.urls'
--- Django实战/hw_mall/myweb/urls.py
+++ Django实战/hw_mall/myweb/urls.py
@@ -1,23 +1,53 @@
-"""hw_mall URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
-
-from .views import t
-
+from . import views,vieworder
 
 urlpatterns = [
-    url(r'^$', t),
+
+    #前台页面
+    url(r'^$', views.index,name="index"),
+    url(r'^index.html$', views.index, name="index"),
+    url(r'^rankindex$',views.indexrank,name="indexrank"),
+    url(r'^list.html$', views.list,name="list"),
+    url(r'^detail/(?P<gid>[0-9]+)$', views.detail,name="detail"),
+
+    #登陆注册页面的路由
+    #跳转登陆页面
+    url(r'^login$', views.login,name="login"),
+    url(r'^dologin$',views.dologin,name="dologin"),
+    url(r'^logout$',views.logout,name="logout"),
+    url(r'^register$', views.register,name="register"),
+    url(r'^doregister$', views.doregister,name="doregister"),
+
+    #购物车页面
+    url(r'^shopcart$', vieworder.shopcart,name="shopcart"),
+    url(r'^addshopcart/(?P<sid>[0-9]+)$',vieworder.addshopcart,name="addshopcart"),
+    url(r'^changeshopcart$',vieworder.changeshopcart,name="changeshopcart"),
+    url(r'^delshopcart/(?P<sid>[0-9]+)$',vieworder.delshopcart,name="delshopcart"),
+    url(r'^clearshopcart$',vieworder.clearshopcart,name="clearshopcart"),
+
+
+    #个人中心页面路由
+    url(r'^personal$', vieworder.personal,name="personal"),
+    url(r'^peredit$',vieworder.peredit,name="peredit"),
+    url(r'^perupdate$',vieworder.perupdate,name="perupdate"),
+
+    #我的订单路由
+    #访问订单表单页
+    url(r'^myorder$', vieworder.myorder,name="myorder"),
+    #订单信息确认
+    url(r'^myorderaffirm$',vieworder.myorderaffirm,name="myorderaffirm"),
+    #订单信息添加
+    url(r'^myorderadd$',vieworder.myorderadd,name="myorderadd"),
+
+    #我的订单
+    url(r'^indent$',vieworder.indent,name="indent"),
+    #取消订单
+    url(r'^indentdel/(?P<oid>[0-9]+)$',vieworder.indentdel,name="indentdel"),
+
+
+
+
+
+
+
 ]
